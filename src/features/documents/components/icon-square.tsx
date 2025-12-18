5c1c3a5cde5a48e0480163428d95dae913884a1b
Okan Ay <okanay@hotmail.com>
2025-12-18 18:00:04 +0300

    dynamic

--- src/features/documents/components/icon-square.tsx
+++ src/features/documents/components/icon-square.tsx
@@ -1,3 +1,3 @@
 export const SquareBullet = () => (
-  <span className="inline-flex size-[9.5px] translate-y-[0.25px] rounded-[1px] bg-primary-heavy" />
+  <span className="inline-flex size-[9.5px] translate-y-[-0.35px] rounded-[1px] bg-primary-heavy" />
 )
